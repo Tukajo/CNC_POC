9d6a6b756d6d0694b477cc47bec777a2f488519d
Timothy Frisch <Tjfrisch@gmail.com>
2014-09-24 11:45:31 -0500

    9-24-14 Commit 11:45 AM
    Push update

---
+++ CNC/src/com/nanospark/cnc/MachineProfile.java
@@ -0,0 +1,17 @@
+package com.nanospark.cnc;
+public class MachineProfile {
+	String profileName;
+
+	public MachineProfile(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+}
